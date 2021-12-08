703a1f51263b46217fa4b2baf357dd2a5057289f
Nishanth Thanayil <nishanthsuresh.thanayilpunathil@vodafoneziggo.com>
2021-12-08 19:40:30 +0100

    updated messages

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="container">
+  <div class="mail-box">
+    <app-sidebar></app-sidebar>
+    <app-content></app-content>
+  </div>
+</div>
+</template>
+
+<script>
+import Sidebar from "./Sidebar.vue";
+import Content from "./Content.vue";
+import messages from "./data/messages";
+export default {
+  data(){
+    return{
+      messages:messages
+    }
+  },
+  components:{
+    appSidebar:Sidebar,
+    appContent:Content
+  }
+}
+</script>
+
+
